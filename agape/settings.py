2cf0af15e05761da258cb93531d3af831b96875f
dotunv <victoroluwasomidotun@gmail.com>
2025-04-29 20:03:08 +0100

    added smtp settings

--- agape/settings.py
+++ agape/settings.py
@@ -491,3 +491,14 @@ if not DEBUG:
 RATELIMIT_ENABLE = True
 RATELIMIT_USE_CACHE = 'default'
 RATELIMIT_KEY_PREFIX = 'ratelimit'
+
+
+# SMTP Settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='your_gmail_address@gmail.com')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='your_gmail_password')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='your_gmail_address@gmail.com')
